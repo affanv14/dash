0f5c7ee8f4a1099dc0d63525bac0930e0708e4f6
affanv14 <affanv14@gmail.com>
2017-02-20 20:56:04 +0530

    add shell builtins

--- shell.c
+++ shell.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+#include<unistd.h>
 #include "utils.h"
+
+#define MAX_PATHNAME_SIZE 1000
 
 int exec_command(char**);
 void init(char**,char***);
+void pwd(void);
+void cd(char*);
 void main()
 {
     char *line;
@@ -33,10 +38,34 @@ int exec_command(char **tokens)
 {
     if(strcmp(tokens[0],"exit")==0)
             return 0;
+    else if(strcmp(tokens[0],"pwd")==0)
+    {
+            pwd();
+            return 1;
+    }
+    else if(strcmp(tokens[0],"cd")==0)
+    {
+        cd(tokens[1]);
+        return 1;
+    }
     else
     {
             printf("command not found\n");
             return 1;
     
     }
+}
+
+void pwd()
+{
+    char *name=getcwd(NULL,MAX_PATHNAME_SIZE);
+    printf("%s\n",name);
+}
+
+void cd(char* path)
+{
+    int status;
+    status=chdir(path);
+    if(status==-1)
+        perror("cd");
 }
